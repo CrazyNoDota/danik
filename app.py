4012b6ead1b060deb55e413210c373142662eca4
daneker <daneker>
2023-04-26 13:49:17 +0000

    new templates

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request
 import openai
-import os
-#oasidfoasidj
+from googletrans import Translator, LANGUAGES
+import time
+
 app = Flask(__name__)
+
+translator = Translator(service_urls=['translate.google.com'])
 
 # Set up OpenAI API credentials
 openai.api_key = "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"
+
+kazakh = False
 
 # Define home page route
 @app.route("/")
@@ -16,17 +21,27 @@ def home():
 @app.route("/generate_text", methods=["POST"])
 def generate_text():
     prompt = request.form["prompt"]
+    
     response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=3000,
-        n=1,
-        stop=None,
-        temperature=0.9,
+    engine="text-davinci-003",
+    prompt=prompt,
+    max_tokens=3000,
+    n=1,
+    stop=None,
+    temperature=0.9,
     )
+   
     text = response.choices[0].text.strip()
     return render_template("generated_text.html", text=text)
 
+@app.route('/update_bool', methods=['POST'])
+def update_bool():
+    global kazakh
+    kazakh = not kazakh
+    if kazakh == False:
+        return 'language set to english plase return'
+    else:
+        return 'The language changed to kazakh please return'
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
-
