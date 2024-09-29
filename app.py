48b2ee7310c2b963de686c439d0b316a4c64a1e4
My name <akJALIL>
2024-09-29 22:36:07 +0500

    still not working

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
+import wave, struct, os
+from openai import OpenAI
 from flask import Flask, render_template, request
-import openai
+client = OpenAI(api_key="sk-TDpzrP8LTYBD5AOVOQtS_BAc_2ipjjOGhHkjJUQsfDT3BlbkFJUbJODhBEsI269PlgbBbu4n3nh7tHPXQDcmGx_TzbAA")
 
-# Initialize the Flask app
 app = Flask(__name__)
-
-# Set up OpenAI API credentials
-openai.api_key = "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"
-
 class Chatbot:
     def __init__(self, client):
         self.client = client
@@ -19,8 +16,8 @@ class Chatbot:
             {"role": "user", "content": message}
         )
         
-        response = self.client.Completion.create(
-            model="gpt-3.5-turbo",
+        response = self.client.chat.completions.create(
+            model="gpt-4-0125-preview",
             messages=self.context
         )
         
@@ -28,12 +25,16 @@ class Chatbot:
         self.context.append(
             {"role": "assistant", "content": response_content}
         )
-        return response_content
+        self.print_chat()
 
-# Initialize the chatbot instance
-chatbot = Chatbot(openai)
+    def print_chat(self):
+        for message in self.context:
+            if message["role"] == "user":
+                return(f'USER: {message["content"]}')
+            elif message["role"] == "assistant":
+                return(f'BOT: {message["content"]}')
 
-# Define home page route
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -41,16 +42,16 @@ def home():
 @app.route("/files.html")
 def serve_files_html():
     return render_template('files.html')
-
-# Define route for generating text using Chatbot
+# Define route for generating text
 @app.route("/generate_text", methods=["POST"])
 def generate_text():
     prompt = request.form["prompt"]
-    
-    # Use the chatbot to generate a response
-    text = chatbot.chat(prompt)
-    
-    return render_template("generated_text.html", text=text)
+    chatbot = Chatbot(client)
+    return render_template("generated_text.html", text=chatbot.chat(prompt))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
+
+
